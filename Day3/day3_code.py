fa98f73ce7a2a298db4bb3309b871a10acfa5e9c
Alexander <35773116+ajhbh@users.noreply.github.com>
2023-12-06 00:49:40 +0000

    Day 3 part 1 refactor

--- Day3/day3_code.py
+++ Day3/day3_code.py
@@ -4,31 +4,43 @@ file = open('Day3/input.txt', 'r')
 lines = file.readlines()
 
 pattern = r'([0-9]+)'
-nonsymbolchars = ['0','1','2','3','4','5','6','7','8','9','.', '\n']
+nonsymbolchars = ['0','1','2','3','4','5','6','7','8','9','.','\n']
+gearchar = '*'
 
-def checkNumberForSymbol(match):
+def checkCellForSymbol(match:re.Match, linestocheck:list[int], columnstocheck:list[int]):
+    for linecheck in linestocheck:
+        for columncheck in columnstocheck:
+            character = lines[linecheck][columncheck]
+            if character not in nonsymbolchars:
+                #print(character, match.group(0))
+                #print(match.group(0))
+                return True
+    return False
+
+def getLinesToCheck(currentlineindex:int):
+    linestocheck = []
+    previouslineindex = currentlineindex - 1
+    nextlineindex = currentlineindex + 1
+    if not(previouslineindex < 0):
+        linestocheck.append(previouslineindex)
+    linestocheck.append(currentlineindex)
+    if not(nextlineindex >= numberoflines):
+        linestocheck.append(nextlineindex)
+    return linestocheck
+
+def getColumnsToCheck(match:re.Match):
     columnstart = match.start() - 1
-    columnend = match.end()
-    columnstocheck = list(range(columnstart, columnend+1))
+    columnend = match.end() + 1
+    columnstocheck = list(range(columnstart, columnend))
     try: 
         columnstocheck.remove(-1)
     except:
         pass
     try:
         columnstocheck.remove(lengthofline)
-        pass
     except:
         pass
-
-    for linecheck in linestocheck:
-        for columncheck in columnstocheck:
-            character = lines[linecheck][columncheck]
-            if character not in nonsymbolchars:
-                print(character, match.group(0))
-                #print(match.group(0))
-                return True
-    return False
-
+    return columnstocheck
 
 sum = 0
 iterator = 0
@@ -39,23 +51,12 @@ for line in lines:
     digitmatcheslist = [x for x in digitmatches]
     #print(digitmatcheslist)
 
-    linestocheck = []
-    previouslineindex = iterator - 1
-    nextlineindex = iterator + 1
-    if not(previouslineindex < 0):
-        linestocheck.append(previouslineindex)
-    linestocheck.append(iterator)
-    if not(nextlineindex >= numberoflines):
-        linestocheck.append(nextlineindex)
-
+    linestocheck = getLinesToCheck(iterator)
     for match in digitmatcheslist:
-        if checkNumberForSymbol(match) == True:
+        columnstocheck = getColumnsToCheck(match)
+        if checkCellForSymbol(match, linestocheck, columnstocheck) == True:
             sum += int(match.group(0))
-
-
-
 
     iterator += 1
 
-
-print("Final sum =", sum)
+print("Part 1 final sum =", sum)
